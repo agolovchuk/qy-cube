9d65ede1c7af37784ae2818a85dea9f1d5c54c65
Alexander Golovchuk <alexander.golovchuk@wmg.com>
2024-09-26 15:53:14 +0200

    Add GH Action

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  base: "/qy-cube/",
+  build: {
+    manifest: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
