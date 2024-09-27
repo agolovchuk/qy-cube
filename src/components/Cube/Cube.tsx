964aca0ecce41011ee9f31b575d3195d5d918010
Alexander Golovchuk <alexander.golovchuk@wmg.com>
2024-09-27 19:48:39 +0200

    Update color

--- src/components/Cube/Cube.tsx
+++ src/components/Cube/Cube.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, MouseEventHandler, useCallback, useRef } from "react";
 import cn from "classnames";
 import { CubeColor, CubeFacet } from "@/lib";
 import "./cube.scss";
@@ -7,9 +7,69 @@ interface Props {
   state?: ReadonlyArray<number>;
 }
 
+type EventHandler = (event: MouseEvent) => void;
+
+// TODO: Update rotation function
+function getRotationParams(
+  position: [number, number, number, number, number, number]
+): [number, number] {
+  const deltaX = position[2] - position[0];
+  const deltaY = position[3] - position[1];
+
+  const sensitivity = 1;
+  position[4] += deltaY * sensitivity;
+  position[5] += deltaX * sensitivity;
+
+  return [position[4], position[5]];
+}
+
 const Cube: FC<Props> = ({ state = [] }) => {
+  const position = useRef<[number, number, number, number, number, number]>([
+    0, 0, 0, 0, 0, 0,
+  ]);
+
+  const container = useRef<HTMLDivElement>(null);
+
+  const updateParams = useCallback(() => {
+    if (container.current) {
+      const [rotateX, rotateY] = getRotationParams(position.current);
+      container.current.style.setProperty("--rotate-x", `${rotateX}deg`);
+      container.current.style.setProperty("--rotate-y", `${rotateY}deg`);
+      position.current[0] = position.current[2];
+      position.current[1] = position.current[3];
+    }
+  }, []);
+
+  const mouseMoveHandler = useCallback<EventHandler>(
+    (event) => {
+      position.current[2] = event.clientX;
+      position.current[3] = event.clientY;
+      window.requestAnimationFrame(updateParams);
+    },
+    [updateParams]
+  );
+
+  const mouseUpHandler = useCallback<EventHandler>(() => {
+    console.log("Mouse UP");
+    document.body.removeEventListener("mousemove", mouseMoveHandler);
+    document.body.removeEventListener("mouseup", mouseUpHandler);
+  }, [mouseMoveHandler]);
+
+  const mouseDownHandler = useCallback<MouseEventHandler>(
+    (event) => {
+      position.current = [event.clientX, event.clientY, 0, 0, 0, 0];
+      document.body.addEventListener("mousemove", mouseMoveHandler, false);
+      document.body.addEventListener("mouseup", mouseUpHandler, false);
+    },
+    [mouseMoveHandler, mouseUpHandler]
+  );
+
   return (
-    <div className="cube__container">
+    <div
+      ref={container}
+      className="cube__container"
+      onMouseDown={mouseDownHandler}
+    >
       <ul className="cube__list">
         {state.map((e, i) => (
           <li
